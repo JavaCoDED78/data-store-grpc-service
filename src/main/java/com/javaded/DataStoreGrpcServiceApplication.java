050f90ed521745181558e5b3a05132c46cb64366
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-06-10 11:58:20 +0300

    init commit

---
+++ src/main/java/com/javaded/DataStoreGrpcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javaded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStoreGrpcServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStoreGrpcServiceApplication.class, args);
+    }
+
+}
